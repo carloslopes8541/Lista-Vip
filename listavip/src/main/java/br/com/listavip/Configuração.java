08a0924c3c2cba46e1b0311603830162e5c9ef0d
carloslopes8541 <carloseduardopereira200@gmail.com>
2020-09-03 00:31:08 -0300

    first commit

---
+++ listavip/src/main/java/br/com/listavip/Configuração.java
@@ -0,0 +1,15 @@
+package br.com.listavip;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Configuração {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Configuração.class, args);
+	}
+		
+
+}
